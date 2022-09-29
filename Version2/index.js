3edf1cec654af3dc6bebfa3a9d8bbb612365ea56
Dustin Koch <dustin.koch@5minds.de>
2022-09-29 14:00:32 +0200

    Worked on code

---
+++ Version2/index.js
@@ -0,0 +1,9 @@
+function switchvisibility() {
+
+    // document.getElementById('test').style.visibility = "hidden";
+    // document.querySelectorAll('#test').style.visibility = "hidden";
+
+    const test = document.querySelectorAll('[test]');
+
+    test.s
+}
